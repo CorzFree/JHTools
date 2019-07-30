81a0706d5091d845b1e4c01dfdb7b688a4f00bbb
Star <1453468914@qq.com>
2019-07-30 16:42:59 +0800

    no message

---
+++ JHTools/JHToolsPloy/channels/klj-YiJie-leju-wbtx/xcode_process.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+mods = {
+    "group": "JHToolsSDK_YiJie",
+    "files": [
+         "libJHToolsSDK_YiJie.a"
+    ],
+    "folders": [
+         "SDK"
+    ],
+    "frameworks": [
+     ],
+    "libs": [
+         "libz.dylib",
+         "libc++.1.dylib",
+         "libz.1.2.5.dylib",
+         "libsqlite3.0.dylib"
+    ]
+}
+
+#URLScheme设置
+def post_process(self, project, infoPlist, sdkparams):
+    self.embed_binary("SDK/OnlineAHelper.framework")
+    infoPlist['com.snowfish.customer'] = "SNOWFISH"
+    infoPlist['com.snowfish.channel'] = "SNOWFISH"
+    infoPlist['com.snowfish.sdk.version'] = "2"
+    infoPlist['com.snowfish.appid'] = "{A8DBF9DF-A724F2C9}"
+    infoPlist['com.snowfish.channelid'] = "{6EDC1D7C-654416C7}"
+    pass
+
+
