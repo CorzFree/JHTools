3f6e5a1e4ed5d09a19e73922c482ddf2ac6bf169
Star <1453468914@qq.com>
2019-08-20 09:08:16 +0800

    no message

--- JHTools/buildscript/iconutils.py
+++ JHTools/buildscript/iconutils.py
@@ -16,7 +16,7 @@ def maskIconImage(imgIcon, imgMark, position):
 
 def get_plugin_dir(pluginRoot, plugin):
     return os.path.join(pluginRoot, 'JHToolsSDK_' + plugin['name'])
-    
+   
 def addIconCorner(folder, channelConfig, pluginRoot):
     for plugin in channelConfig['plugins']:
         if not "IconCorner" in plugin:
@@ -36,3 +36,6 @@ def addIconCorner(folder, channelConfig, pluginRoot):
             iconImage = Image.open(iconFile)
             iconImage = maskIconImage(iconImage, iconMark, (0,0))
             iconImage.save(iconFile, 'PNG')
+
+
+        
