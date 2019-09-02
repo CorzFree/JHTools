bde40eb6656670f2f4781b85cd5ff16b0395d9f0
Star <1453468914@qq.com>
2019-09-02 10:19:57 +0800

    no message

--- JHTools/JHToolsPloy/channels/txlt-biubiupk-tlszhycs/ChannelResource/packconfig.lua
+++ JHTools/JHToolsPloy/channels/txlt-biubiupk-tlszhycs/ChannelResource/packconfig.lua
@@ -10,3 +10,10 @@ IS_ADAPTER_IPHONEX = true
 LUA_VERSION = 1
 --SDK渠道
 SDK_CHANNEL = 669
+
+--版号
+PAGE_NUM = "出版单位：上海都玩网络科技有限公司  批准文号：新广出审[2016]2352号  出版物号：ISBN 978-7-7979-0939-6  著作权人：上海辛辣网络科技有限公司"
+
+--伪版本号
+PKG_VERSION = 2
+
